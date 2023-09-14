8472c4b9b164a35962bc1350c5e9e7677cffcb13
Harsh Mittal <harshmittal2210@gmail.com>
2023-09-14 22:16:12 +0530

    Added Whitespace and Text Highlighting

--- scripts/test_basic.py
+++ scripts/test_basic.py
@@ -1,15 +1,70 @@
 import sys
 import pygame
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QTextEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QPlainTextEdit
+from PyQt5.QtGui import QSyntaxHighlighter, QTextCursor, QTextCharFormat
+from PyQt5.Qsci import QsciScintilla, QsciLexerPython
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+
+class PythonCodeEditor(QsciScintilla):
+    def __init__(self):
+        super().__init__()
+
+        # Create a lexer for Python
+        lexer = QsciLexerPython(self)
+        self.setLexer(lexer)
+
+        # Set up tab and indentation settings
+        self.setTabWidth(4)
+        self.setIndentationsUseTabs(False)
+        self.setIndentationGuides(True)
+        self.setAutoIndent(True)
+
+        # Create a syntax highlighter
+        self.highlighter = PythonSyntaxHighlighter(self)
+
+    def setPlainText(self, text):
+        super().setPlainText(text)
+        self.setCursorPosition(0, 0)
+
+
+class PythonSyntaxHighlighter(QSyntaxHighlighter):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.highlighting_rules = []
+
+        # Create rules for highlighting different elements
+        keyword_format = QTextCharFormat()
+        keyword_format.setForeground(Qt.blue)
+        keyword_format.setFontWeight(QFont.Bold)
+        keywords = ["if", "else", "while", "for", "def", "import", "as", "print"]  # Add more as needed
+        for keyword in keywords:
+            rule = (r'\b' + keyword + r'\b', keyword_format)
+            self.highlighting_rules.append(rule)
+
+        # Create a rule for strings (in double or single quotes)
+        string_format = QTextCharFormat()
+        string_format.setForeground(Qt.darkGreen)
+        self.highlighting_rules.append((r'\".*\"', string_format))
+        self.highlighting_rules.append((r'\'.*\'', string_format))
+
+    def highlightBlock(self, text):
+        for pattern, format in self.highlighting_rules:
+            expression = QRegExp(pattern)
+            index = expression.indexIn(text)
+            while index >= 0:
+                length = expression.matchedLength()
+                self.setFormat(index, length, format)
+                index = expression.indexIn(text, index + length)
 
 
 class PygameRunner(QWidget):
     def __init__(self):
         super().__init__()
 
-        # Create a QTextEdit widget for entering Python code
-        self.code_editor = QTextEdit(self)
-        self.code_editor.setPlaceholderText("Enter Pygame code here...")
+        # Create a Python code editor widget
+        self.code_editor = PythonCodeEditor()
 
         # Create a "Run" button
         self.run_button = QPushButton("Run", self)
@@ -26,8 +81,12 @@ class PygameRunner(QWidget):
         self.setLayout(layout)
 
     def run_pygame_code(self):
-        # Get the Python code from the QTextEdit widget
-        pygame_code = self.code_editor.toPlainText()
+        # Clear the Pygame screen if it's already open
+        if self.screen:
+            pygame.quit()
+
+        # Get the Python code from the code editor
+        pygame_code = self.code_editor.text()
 
         # Initialize Pygame
         pygame.init()
