5802c1dbafa3ad8e21fe91778afab7138fc73454
Harsh Mittal <harsh.mittal@braneenterprises.com>
2023-09-28 01:31:49 +0530

    Lab8 Added

---
+++ Tutorials/6_Game/lab6.py
@@ -0,0 +1,78 @@
+import pygame
+from dinoGame.elements import Track, Dinosaur, Cloud
+from dinoGame.elements import LargeCactus, Bird, SmallCactus
+
+# Initialize Pygame
+pygame.init()
+
+SCREEN_HEIGHT = 600  # Enter the window Height
+SCREEN_WIDTH = 1100   # Enter the window Width
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+alive = True
+
+while alive:
+
+    SCREEN.fill((255, 255, 255))
+    font = pygame.font.Font('freesansbold.ttf', 30)
+    text = font.render("Press any Key to Start", True, (0, 0, 0))
+    textRect = text.get_rect()
+    textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 3)
+    SCREEN.blit(text, textRect)
+
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            alive = False
+        elif event.type == pygame.KEYDOWN:
+            ## Start Game
+            ground = Track(SCREEN)
+            dino = Dinosaur(SCREEN)
+            cloud = Cloud(SCREEN)
+            clock = pygame.time.Clock()
+            obstacle = None ## Empty Obstacle
+
+            run = True
+            while run:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        run = False
+                        continue
+                
+                if obstacle == None:
+                    obstacle = SmallCactus(SCREEN)
+
+                ## Update The Elements
+                user_input = pygame.key.get_pressed()
+                dino.update(user_input)
+                ground.update()
+                cloud.update()
+                obstacle.update()
+
+                ### Draw All Layers
+                SCREEN.fill((255,255,255))
+                ground.draw()
+                dino.draw()
+                cloud.draw()
+                obstacle.draw()
+
+
+
+                ##########Detect Collision############
+                #if dino.dino_rect.colliderect(obstacle.rect):
+                #    break
+                # dino.dino_rect.right          obstacle.rect.right
+                # dino.dino_rect.left           obstacle.rect.left
+                # dino.dino_rect.top            obstacle.rect.top
+                # dino.dino_rect.bottom         obstacle.rect.bottom
+
+                ######################################
+
+
+                if obstacle.rect.x <= -obstacle.rect.width:
+                    obstacle = None
+
+                clock.tick(30)
+                pygame.display.update()
+pygame.quit()
---
+++ Tutorials/6_Game/lab8.py
@@ -0,0 +1,85 @@
+import pygame
+from dinoGame.elements import Track, Dinosaur, Cloud
+from dinoGame.elements import LargeCactus, Bird, SmallCactus
+
+# Initialize Pygame
+pygame.init()
+
+SCREEN_HEIGHT = 600  # Enter the window Height
+SCREEN_WIDTH = 1100   # Enter the window Width
+SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+alive = True
+
+total_score = 0
+
+while alive:
+
+    SCREEN.fill((255, 255, 255))
+    font = pygame.font.Font('freesansbold.ttf', 30)
+    text = font.render("Press any Key to Start", True, (0, 0, 0))
+    textRect = text.get_rect()
+    textRect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 3)
+    SCREEN.blit(text, textRect)
+
+    text_score = font.render(f"Score: {total_score}", True, (0, 0, 0))
+    score_Rect = text_score.get_rect()
+    score_Rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 1.5)
+    SCREEN.blit(text_score, score_Rect)
+
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            alive = False
+        elif event.type == pygame.KEYDOWN:
+            ## Start Game
+            ground = Track(SCREEN)
+            dino = Dinosaur(SCREEN)
+            cloud = Cloud(SCREEN)
+            clock = pygame.time.Clock()
+            obstacle = None
+
+            game_score = 0
+
+            run = True
+            while run:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        run = False
+                        continue
+                ##########Have Different Obstacles#########
+                if obstacle == None:
+                    obstacle = LargeCactus(SCREEN)
+                ###########################################
+
+                ## Update The Elements
+                user_input = pygame.key.get_pressed()
+                dino.update(user_input)
+                ground.update()
+                cloud.update()
+                obstacle.update()
+
+                ### Draw All Layers
+                SCREEN.fill((255,255,255))
+                ground.draw()
+                dino.draw()
+                cloud.draw()
+                obstacle.draw()
+
+                if dino.dino_rect.colliderect(obstacle.rect):
+                    total_score  = game_score
+                    break
+
+                if obstacle.rect.x <= -obstacle.rect.width:
+                    obstacle = None
+                    game_score += 1
+
+                text = font.render("Points: " + str(game_score), True, (0, 0, 0))
+                textRect = text.get_rect()
+                textRect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/4)
+                SCREEN.blit(text, textRect)
+
+                clock.tick(30)
+                pygame.display.update()
+pygame.quit()
