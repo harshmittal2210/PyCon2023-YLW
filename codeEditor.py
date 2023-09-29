f249387a1979b4cb5092053f04f7decea9051fcc
Harsh Mittal <harsh.mittal@braneenterprises.com>
2023-09-30 02:27:33 +0530

    Updated Lib

--- codeEditor.py
+++ codeEditor.py
@@ -1,54 +1,36 @@
-from PyQt5.Qsci import QsciScintilla, QsciLexerPython
-from PyQt5.QtGui import QSyntaxHighlighter, QTextCharFormat
+from PyQt5.QtGui import QTextCursor, QTextCharFormat, QSyntaxHighlighter, QFont
+from PyQt5.QtWidgets import QPlainTextEdit, QTextEdit, QWidget, QVBoxLayout, QPushButton, QApplication
 from PyQt5.QtCore import Qt, QRegExp
-from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QApplication
 
-
-class PythonCodeEditor(QsciScintilla):
+class PythonCodeEditor(QPlainTextEdit):
     def __init__(self):
         super().__init__()
 
-        # Create a lexer for Python
-        lexer = QsciLexerPython(self)
-        self.setLexer(lexer)
-
         # Set up tab and indentation settings
-        self.setTabWidth(4)
-        self.setIndentationsUseTabs(False)
-        self.setIndentationGuides(True)
-        self.setAutoIndent(True)
+        self.setTabStopWidth(20)
+        self.setTabChangesFocus(False)
 
         # Create a syntax highlighter
-        self.highlighter = PythonSyntaxHighlighter(self)
-
-        # font = QFont()
-        # screen_dpi = QApplication.primaryScreen().logicalDotsPerInch()
-        # font_size = int(screen_dpi / 96 * 16)
-        # font.setPixelSize(font_size)
-        # self.setFont(font)
+        self.highlighter = PythonSyntaxHighlighter(self.document())
 
         self.zoom_level = 5
-        self.zoomTo(self.zoom_level)
     
     def textZoomIn(self):
         self.zoom_level *= 2
         self.zoom_level = min(self.zoom_level, 20)
-        self.zoomTo(self.zoom_level)
-    
+        self.zoomIn(self.zoom_level)
+
     def textZoomOut(self):
         self.zoom_level /= 2
         self.zoom_level = max(self.zoom_level, 2)
-        self.zoomTo(self.zoom_level)
-
-    def setPlainText(self, text):
-        self.setText(text)
-        self.setCursorPosition(0, 0)
-
+        self.zoomOut(self.zoom_level)
+    
+    def text(self):
+        return self.toPlainText()
 
 class PythonSyntaxHighlighter(QSyntaxHighlighter):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self, document):
+        super().__init__(document)
 
         self.highlighting_rules = []
 
@@ -56,7 +38,9 @@ class PythonSyntaxHighlighter(QSyntaxHighlighter):
         keyword_format = QTextCharFormat()
         keyword_format.setForeground(Qt.blue)
         keyword_format.setFontWeight(QFont.Bold)
-        keywords = ["if", "else", "while", "for", "def", "import", "as", "print"]  # Add more as needed
+        keywords = ["if", "else", "while", "for", "def", 
+                    "import", "as", "print", "from", "elif",
+                    "len", "not", "is", "and", "else"]  # Add more as needed
         for keyword in keywords:
             rule = (r'\b' + keyword + r'\b', keyword_format)
             self.highlighting_rules.append(rule)
@@ -66,6 +50,7 @@ class PythonSyntaxHighlighter(QSyntaxHighlighter):
         string_format.setForeground(Qt.darkGreen)
         self.highlighting_rules.append((r'\".*\"', string_format))
         self.highlighting_rules.append((r'\'.*\'', string_format))
+        self.highlighting_rules.append((r'\#.*\'', string_format))
 
     def highlightBlock(self, text):
         for pattern, format in self.highlighting_rules:
@@ -75,3 +60,12 @@ class PythonSyntaxHighlighter(QSyntaxHighlighter):
                 length = expression.matchedLength()
                 self.setFormat(index, length, format)
                 index = expression.indexIn(text, index + length)
+
+if __name__ == '__main__':
+    app = QApplication([])
+
+    editor = PythonCodeEditor()
+    editor.show()
+
+    app.exec_()
+
